0685c09e1e286a4e650d4c63e9959c488e1da166
Shruti <priyashruti2002@gmail.com>
2023-12-25 14:26:10 +0530

    deploy

---
+++ frontend/src/pages/user/UserOrderDetailsPage.js
@@ -0,0 +1,9 @@
+import UserOrderDetailsPageComponent from "./components/UserOrderDetailsPageComponent";
+import { useSelector } from "react-redux";
+
+const UserOrderDetailsPage = () => {
+    const userInfo = useSelector((state) => state.userRegisterLogin.userInfo);
+  return <UserOrderDetailsPageComponent userInfo={userInfo} />;
+};
+
+export default UserOrderDetailsPage;
